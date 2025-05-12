e61929d5a339c945638b7fe8e27e6f103e7af175
Cristobal <cmenesesveliz@gmail.com>
2025-05-12 03:14:16 -0400

    añadidas funciones de checkeo y solicitudes al usuario para ingresar datos.

--- lectura.py
+++ lectura.py
@@ -1,9 +1,34 @@
 import subprocess
 import os
+import getpass
+import requests
+import tkinter as tk
+from tkinter import simpledialog
+
 hostname = subprocess.check_output("hostname", shell=True, text=True).strip()
 ruta_archivo = os.path.join(os.getcwd(), f"{hostname}_resultado_checkeo.txt")
+
 with open(ruta_archivo, "w", encoding="utf-8") as f:
-    f.write("Información de registro del equipo: " + hostname + "\n\n")
+    f.write("Información de registro del equipo: " + hostname + "\n")
+def pedirAnexo():
+    root = tk.Tk()
+    root.withdraw()
+    anexo = simpledialog.askstring("ANEXO DEL EQUIPO","Ingrese el Anexo que corresponde a este equipo")
+    print("Ingresado el anexo: "+anexo)
+    return anexo
+checkAnexo = pedirAnexo()
+with open(ruta_archivo, "a", encoding="utf-8") as f:
+    f.write("\n ==== ANEXO ==== \n\n" + checkAnexo +"\n")
+
+def pedirPassword():
+    root = tk.Tk()
+    root.withdraw()
+    contrasena = simpledialog.askstring("CONTRASEÑA DEL EQUIPO", "Ingrese la contraseña de este equipo")
+    print("Contraseña guardada")
+    return contrasena
+checkPassword = pedirPassword()
+with open(ruta_archivo,"a",encoding="utf-8") as f:
+    f.write("\n === CONTRASEÑA DEL EQUIPO === \n\n"+checkPassword+"\n")
 def checkIP():
     try:
         check_ip = subprocess.check_output("ipconfig",shell=True,text=True)
@@ -15,19 +40,23 @@ def checkIP():
                 if value:
                     print("Dirección IPv4: "+value)
                     with open(ruta_archivo, "a", encoding="utf-8") as f:
-                        f.write("\n===== DIRECCION IP =====\n"+value)
+                        f.write("\n===== DIRECCION IP =====\n\n"+value+"\n")
                 else:
                     with open(ruta_archivo, "a", encoding="utf-8") as f:
                         f.write("\n===== DIRECCION IP NO ENCONTRADA =====")
     except subprocess.CalledProcessError as e:
         print("error en la ejecucion del comando CMD")
         print(e)
+def checkProfile():
+    check_user = getpass.getuser()
+    with open(ruta_archivo,"a",encoding="utf-8") as f:
+        f.write("\n==== Perfil en el equipo ====\n\n"+check_user+"\n")
 def checkAdmin():
 
 
     try:
         check_admin = subprocess.check_output("powershell -Command \"[bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)\"",text=True)
-        if check_admin.strip() == True:
+        if check_admin.strip() == "True":
             print("Es administrador")
             with open(ruta_archivo, "a", encoding="utf-8") as f:
                 f.write("\n==== ¿ES ADMINISTRADOR? ====\n")
@@ -95,25 +124,25 @@ def checkMac():
                 if value:
                     print("Tiene direccion mac: "+value)
                     with open(ruta_archivo, "a", encoding="utf-8") as f:
-                        f.write("\n===== DIRECCION MAC =====\n "+value)
+                        f.write("\n===== DIRECCION MAC =====\n\n "+value+"\n")
                 else:
                     print("No se ha encontrado dirección MAC")
                     with open(ruta_archivo, "a", encoding="utf-8") as f:
-                        f.write("\nDireccion mac no encontrada en el equipo")
+                        f.write("\nDireccion mac no encontrada en el equipo\n")
     except subprocess.CalledProcessError as e:
         print("Error checkeando direccion MAC\n"+e)
 def checkHost():
     try:
         check_hostname = subprocess.check_output("hostname",shell=True,text=True)
         with open(ruta_archivo, "a", encoding="utf-8") as f:
-            f.write("\n===== HOSTNAME =====\n" + check_hostname)
+            f.write("\n===== HOSTNAME =====\n\n" + check_hostname)
     except subprocess.CalledProcessError as e:
         print("error checkeando Hostname del equipo\n" + e)
 def checkSerial():
     try:
         check_serialnumber = subprocess.check_output("wmic bios get serialnumber",text=True,shell=True)
         with open(ruta_archivo, "a", encoding="utf-8") as f:
-            f.write("\n===== SERIAL NUMBER =====\n"+check_serialnumber)
+            f.write("\n===== SERIAL NUMBER =====\n" + check_serialnumber.strip() + "\n")
     except subprocess.CalledProcessError as e:
         print("Error leyendo el serialnumber del equipo\n"+e)
 def check_app_instalada(nombre_app):
@@ -143,20 +172,66 @@ def check_app_instalada(nombre_app):
 
     print(f"{nombre_app} NO está instalada.")
     return False
+def checkYTB():
+    try:
+        check_ytb = requests.get("https://www.youtube.com", timeout=5)
+        if check_ytb.status_code == 200:
+            print("El equipo tiene acceso a Youtube")
+            with open(ruta_archivo,"a",encoding="utf-8") as f:
+                f.write("\n==== Acceso a YTB ====\n \nSI tiene acceso a Youtube!!\n")
+            return True
+        if check_ytb.status_code == 500:
+            with open(ruta_archivo, "a", encoding="utf-8") as f:
+                f.write("\n==== Acceso a YTB ====\n \nNO tiene acceso a Youtube\n")
+    except requests.RequestException as e:
+        print("No se tiene acceso a Youtube: ",e)
+        return False
+def checkTipoEquipo():
+    try:
+        tipo = subprocess.check_output(
+            'powershell -Command "Get-WmiObject -Class Win32_SystemEnclosure | Select-Object -ExpandProperty ChassisTypes"',
+            shell=True,
+            text=True
+        )
 
+        tipo = tipo.strip().replace("{", "").replace("}", "")
+        lineas = tipo.splitlines()
 
+        chasis = []
+        for linea in lineas:
+            linea = linea.strip()
+            if linea.isdigit():
+                chasis.append(int(linea))
 
-###CHECK CMD###
-checkIP()
-checkHost()
-checkMac()
-checkSerial()
-###CHECK OPCIONES DE WINDOWS###
-checkAdmin()
-checkRDP()
-checkFirewall()
-# APLICACION INSTALADA
-check_app_instalada("Google Chrome")
+        descripcion = {
+            3: "Desktop/CPU",
+            4: "Low Profile Desktop",
+            6: "Mini Tower",
+            7: "Tower",
+            13: "All-in-One/AIO",
+            9: "Laptop",
+            10: "Notebook"
+        }
+
+        tipo_detectado = [descripcion.get(c, f"Desconocido ({c})") for c in chasis]
+
+        with open(ruta_archivo, "a", encoding="utf-8") as f:
+            f.write("\n==== TIPO DE EQUIPO ====\n\n")
+            f.write("\n\n".join(tipo_detectado) + "\n")
+
+        print("Tipo de equipo detectado:", ", ".join(tipo_detectado))
+
+    except Exception as e:
+        print("Error detectando el tipo de equipo:", e)
+
+###for que ejecuta cada funcion previa y evita que el script se detenga si falla una
+for func in [checkIP, checkHost, checkMac, checkSerial, checkAdmin, checkRDP, checkFirewall, checkProfile,
+             checkYTB, checkTipoEquipo,lambda: check_app_instalada("Topia.exe")]:
+    try:
+        func()
+    except Exception as e:
+        print(f"Error ejecutando {func.__name__}: {e}")
+
 
 
 
