6cfb3cac131b4941a9f7c1202067a71b6a2ba2e8
Cristobal <cmenesesveliz@gmail.com>
2025-06-08 19:02:19 -0400

    ajustada la ruta que busca el excel para utilizarlo sin empaquetarlo en el script

--- core/ruta_excel.py
+++ core/ruta_excel.py
@@ -1,9 +1,11 @@
-import os
 import sys
+import os
 
 if getattr(sys, 'frozen', False):
-    ruta_base = sys._MEIPASS if hasattr(sys, '_MEIPASS') else os.path.dirname(sys.executable)
+    # Carpeta donde está el EXE que se usa en otros equipos!!
+    ruta_base = os.path.dirname(sys.executable)
 else:
+    # Carpeta donde está el .py archivo principal para ejecución
     ruta_base = os.path.dirname(os.path.abspath(__file__))
 
 archivo_vicarius = os.path.join(ruta_base, 'vicarius.xlsx')
